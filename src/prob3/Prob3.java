83fea5ddc8215d19242ef0941fd3e0671cb7f9c2
yattadog <kickscar@gmail.com>
2016-09-19 12:51:44 +0900

    1st commit

---
+++ src/prob3/Prob3.java
@@ -0,0 +1,14 @@
+package prob3;
+
+public class Prob3 {
+
+	public static void main(String[] args) {
+		CurrencyConverter.setRate( 1000 );
+		
+		double dollar = CurrencyConverter.toDollar( 1000000 );
+		System.out.println( "백만원은 " + dollor + "달러입니다." );
+		
+		double krw = CurrencyConverter.toKRW( 100 );
+		System.out.println( "백달러는 " + krw + "원 입니다." );		
+	}
+}
---
+++ src/prob4/CalcApp.java
@@ -0,0 +1,8 @@
+package prob4;
+
+public class CalcApp {
+
+	public static void main(String[] args) {
+		// Scanner 와  String 클래스의 split 함수를 사용합니다.
+	}
+}
